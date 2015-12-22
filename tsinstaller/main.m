92eda4bcaca868d9e2e248ae15cc1c7fc9a94c6f
i_82 <i.82@qq.com>
2015-12-22 22:29:49 +0800

    Second Commit

--- tsinstaller/main.m
+++ tsinstaller/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NSLog(@"%@", NSHomeDirectory());
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
